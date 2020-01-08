2d94381481711f54be2ab95eb6430340d0004d41
Alex <caesar2050@outlook.com>
2020-01-08 12:46:29 +0800

    Review added

--- books/admin.py
+++ books/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Books
+from .models import Books, Review
+
+class ReviewInline(admin.TabularInline):
+    model = Review
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title","author","price",)
-
+    inlines = [
+        ReviewInline,
+    ]
 admin.site.register(Books, BookAdmin)
